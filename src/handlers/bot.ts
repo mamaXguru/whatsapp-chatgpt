fc727209a6f2f25ba499230fc0bbb737f2b8734f
Hareesh Kumar <hareesh@live.in>
2023-05-14 09:42:38 +0530

    config change

--- src/handlers/bot.ts
+++ src/handlers/bot.ts
@@ -22,15 +22,15 @@ const handleMessageGPT = async (message: Message, prompt: string) => {
 
 		const end = Date.now() - start;
 		// const response = `[GPT] Msg recieved ${message.from}: ${prompt}`
-		const response = await axios.post('http://0.0.0.0:8000/api/journal/chat', {
-			"message": prompt,
-			"user_id": message.from
-		})
-
-		// const response = await axios.post('https://api.mamaguru.co/api/chat/chat', {
+		// const response = await axios.post('http://0.0.0.0:8000/api/chat/chat', {
 		// 	"message": prompt,
 		// 	"user_id": message.from
 		// })
+
+		const response = await axios.post('https://api.mamaguru.co/api/chat/chat', {
+			"message": prompt,
+			"user_id": message.from
+		})
 
 		cli.print(`[GPT] Answer to ${message.from}: ${response.data.msg}  | OpenAI request took ${end}ms)`);
 
@@ -54,15 +54,15 @@ const handleVoiceMessageGPT = async (message: Message, prompt: string) => {
 
 		const end = Date.now() - start;
 		// const response = `[GPT] Msg recieved ${message.from}: ${prompt}`
-		const response = await axios.post('http://0.0.0.0:8000/api/chat/voice_chat', {
-			"message": prompt,
-			"user_id": message.from
-		})
-
-		// const response = await axios.post('https://api.mamaguru.co/api/chat/chat', {
+		// const response = await axios.post('http://0.0.0.0:8000/api/journal/chat', {
 		// 	"message": prompt,
 		// 	"user_id": message.from
 		// })
+
+		const response = await axios.post('https://api.mamaguru.co/api/journal/chat', {
+			"message": prompt,
+			"user_id": message.from
+		})
 
 		cli.print(`[GPT] Answer to ${message.from}: ${response.data.msg}  | OpenAI request took ${end}ms)`);
 
