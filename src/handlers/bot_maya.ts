684f6496bf0e3560234a3baee5d946eab310bb4a
Hareesh Kumar <hareesh@live.in>
2023-06-13 14:37:48 +0530

    adding delay

--- src/handlers/bot_maya.ts
+++ src/handlers/bot_maya.ts
@@ -30,7 +30,7 @@ const handleMessageMaya = async (message: Message, prompt: string) => {
 			"message": prompt,
 			"user_id": message.from
 		})
-		await delay(5000);
+		const temp_delay = await delay(10000);
 		const end = Date.now() - start;
 		cli.print(`[GPT] Answer to ${message.from}: ${response.data.msg}  | OpenAI request took ${end}ms)`);
 
